898552ac1109196bfe27f482dca39fb29bacfcc5
nourhanasharf <nourhanashraf142004@gmail.com>
2025-08-08 23:37:45 +0300

    noura

---
+++ app/Models/MedicalFile.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MedicalFile extends Model
+{
+    protected $fillable = ['file_path', 'note', 'file_type', 'family_code'];
+}
